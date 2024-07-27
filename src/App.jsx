e0368eb1e8a7c0f5fe746b7a0a5779913daf0122
Darklight-ui <henr9926@gmail.com>
2024-07-27 09:10:45 +0100

    sync

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,38 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Coins from "./components/Coins";
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainLayOuts from "./Layouts/MainLayOuts";
 import Coin from "./Pages/Coin";
 
 const App = () => {
   const [coins, setCoins] = useState([]);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1'
-  useEffect(() =>{
-    axios.get(url).then((response) =>{
-      setCoins(response.data)
-      setLoading(false)
-      console.log(response.data[0])
-    }).catch((error) =>{
-      console.log(error)
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1';
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
       setLoading(false);
-    })
-  }, [])
+      console.log(response.data[0]);
+    }).catch((error) => {
+      console.log(error);
+      setLoading(false);
+    });
+  }, []);
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/"  element={<MainLayOuts />}>
-        <Route index element={<Coins coins={coins} loading={loading} />} />
-        <Route path="/coin/:id" element={<Coin />} />
-      </Route>
-    )
-  )
-  return ( 
-    <div className="bg-bgColor min-h-screen text-offWhite">
-      <RouterProvider router={router} />
-    </div>
-   )
-}
+  return (
+    <Router basename="/crypto-app/">
+      <div className="bg-bgColor min-h-screen text-offWhite">
+        <Routes>
+          <Route path="/" element={<MainLayOuts />}>
+            <Route index element={<Coins coins={coins} loading={loading} />} />
+            <Route path="/coin/:id" element={<Coin />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/components/CoinInfo.jsx
+++ src/components/CoinInfo.jsx
@@ -4,24 +4,25 @@ import React from 'react'
 const CoinInfo = ({coin}) => {
   return (
     <>
-       <div className="container flex flex-col mx-auto max-w-[700px] w-full overflow-auto">
+       <div className="container flex flex-col mx-auto max-w-fit w-full overflow-x-scroll
+       ">
       <div className="">
-        <p className="flex items-center text-center justify-center my-3 mx-auto text-2xl font-semibold uppercase p-3 shadow-xl md:w-[700px] w-[500px]">{coin.name}</p>
+        <p className="flex items-center text-center justify-center my-3 mx-auto text-2xl font-semibold uppercase p-3 shadow-xl md:w-[700px] w-[300px]">{coin.name}</p>
         </div>
-      <div className="flex mx-auto uppercase md:w-[700px] w-[500px]">
+      <div className="flex mx-auto uppercase md:w-[700px] w-[300px]">
         <span className="bg-bgPurple w-fit my-2 p-1 rounded font-semibold text-sm">Rank #{coin.market_cap_rank}</span>
       </div>
-      <div className="container flex mx-auto justify-between md:w-[700px] w-[500px] ">
+      <div className="container flex mx-auto justify-between md:w-[700px] w-[300px] ">
         <div className="coin-heading flex gap-2">
           <p>{coin.image ? <img src={coin.image.small} alt='' width={'25px'} /> : null}</p>
           <p>{coin.name ? coin.name : null}</p>
           <p>{coin.symbol ? coin.symbol : null}</p>
         </div>
         <div className="">
-          <p className="font-semibold text-3xl">{coin.market_data ? <p>${coin.market_data.current_price.usd.toLocaleString()}</p> : null}</p>
+          <p className="font-semibold text-xl md:text-3xl">{coin.market_data ? <p>${coin.market_data.current_price.usd.toLocaleString()}</p> : null}</p>
         </div>
       </div>
-        <div className="flex items-center text-center justify-evenly my-3 mx-auto text-sm font-semibold uppercase p-3 shadow-xl md:w-[700px] w-[500px]">
+        <div className="flex items-center text-center justify-evenly my-3 mx-auto text-sm font-semibold uppercase p-3 shadow-xl md:w-[700px] w-[350px]">
           <table className='border-spacing-2 w-full border-slate-500'>
             <thead>
               <tr >
@@ -34,7 +35,7 @@ const CoinInfo = ({coin}) => {
               </tr>
             </thead>
             <tbody>
-              <tr className='font-normal'>
+              <tr className='font-normal text-xs md:text-xl border-spacing-2'>
                 <td>{coin.market_data?.price_change_percentage_1h_in_currency ? <p>{coin.market_data.price_change_percentage_1h_in_currency.usd}</p> : null}</td>
                 <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_24h_in_currency.usd}</p> : null}</td>
                 <td>{coin.market_data?.price_change_percentage_7d_in_currency ? <p>{coin.market_data.price_change_percentage_7d_in_currency.usd}</p> : null}</td>
@@ -46,11 +47,11 @@ const CoinInfo = ({coin}) => {
           </table>
         </div>
         <div className="flex flex-col items-center text-center justify-center my-3 mx-auto ">
-          <div className="flex justify-between mx-auto w-full md:max-w-[700px] gap-3 p-3 my-3 shadow-xl">
-            <div className="flex flex-col w-fit">
+          <div className="flex flex-col md:flex-row justify-between mx-auto w-[300px] md:w-[700px] gap-3 p-3 my-3 shadow-xl">
+            <div className="flex flex-col">
               <div className="flex p-3 justify-between gap-3 border-b my-3">
               <p className='font-semibold font-Ubuntu'>24 Hour Low</p>
-              <p>{coin.market_data?.low_24h.usd.low_24h ? <p>{coin.market_data.low_24h.usd}</p> : null}</p>
+              <p>{coin.market_data?.low_24h ? <p>{coin.market_data.low_24h.usd}</p> : null}</p>
               </div>
               <div className="flex p-3 justify-between gap-3 border-b my-3">
               <p className='font-semibold font-Ubuntu'>24 Hour High</p> 
@@ -68,12 +69,10 @@ const CoinInfo = ({coin}) => {
               </div>
             </div>
           </div>
-          <div className="p-3 shadow-xl md:w-[700px] w-[500px]">
-            <div className="about">
-              <p className='font-semibold text-3xl p-3 font-Rubik uppercase'>About</p> 
-              <p className='text-lg text-justify' dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(coin.description ? coin.description.en : '')}}></p>
+          <div className="p-3 shadow-xl md:w-[700px] w-[300px]">
+              <p className='font-semibold text-lg p-3 font-Rubik uppercase'>About</p> 
+              <p className='text-sm text-justify' dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(coin.description ? coin.description.en : '')}}></p>
             </div>
-          </div>
         </div>
     </div> 
     </>
